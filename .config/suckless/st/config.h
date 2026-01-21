5ab051f795a12031a67d390d7562f5aed9cc6738
t <252655163+torsbos@users.noreply.github.com>
2026-01-21 15:45:13 +0000

    Change font to IBM Plex Mono

--- .config/suckless/st/config.h
+++ .config/suckless/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "VGA:size=14";
+static char *font = "IBM Plex Mono:pixelsize=12";
 static int borderpx = 2;
 
 /*
