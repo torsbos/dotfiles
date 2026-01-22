ddd2cde964a6dd3994083c9d9585d422fbb6dbec
t <252655163+torsbos@users.noreply.github.com>
2026-01-22 08:05:17 +0000

    Add qute

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,17 @@
+config.load_autoconfig(False)
+config.set('content.cookies.accept', 'never', 'chrome-devtools://*')
+config.set('content.cookies.accept', 'never', 'devtools://*')
+config.set('content.javascript.enabled', False, 'chrome-devtools://*')
+config.set('content.javascript.enabled', False, 'devtools://*')
+config.set('content.javascript.enabled', False, 'chrome://*/*')
+config.set('content.javascript.enabled', False, 'qute://*/*')
+c.url.default_page = 'about:blank'
+c.url.searchengines = {
+    'DEFAULT': 'https://searxng.site/searxng/?q={}', 
+    'aw': 'https://wiki.archlinux.org/?search={}', 
+    'w': 'https://en.wikipedia.org/?search={}', 
+    'ub' : 'https://gu-se-primo.hosted.exlibrisgroup.com/primo-explore/search?query=any,contains,{}&vid=46GUB_VU1&search_scope=default_scope&sortby=rank&lang=sv_SE',
+    'r': 'https://old.reddit.com/r/{}'
+}
+c.url.start_pages = 'about:blank'
+c.colors.webpage.darkmode.enabled = True
