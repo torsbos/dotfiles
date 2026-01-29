40fc0c04fe0dec3ce014e4e04ad40140faa7373b
t <252655163+torsbos@users.noreply.github.com>
2026-01-29 15:50:42 +0100

    Change font to Terminus

--- .config/suckless/st/config.h
+++ .config/suckless/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "IBM Plex Mono:pixelsize=12";
+static char *font = "Terminus:pixelsize=16";
 static int borderpx = 2;
 
 /*
