87340f40877712714e2fdbbcddb0346f386defa6
t <252655163+torsbos@users.noreply.github.com>
2026-02-06 20:51:34 +0100

    edit binds for spawning st and librewolf + recompile

--- .config/suckless/dwm/config.h
+++ .config/suckless/dwm/config.h
@@ -66,7 +66,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray2, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *slockcmd[]  = { "/usr/local/bin/slock", NULL };
-static const char *qutecmd[]  = { "/usr/bin/qutebrowser", NULL };
+/*static const char *qutecmd[]  = { "/usr/bin/qutebrowser", NULL };*/
 static const char *librecmd[]  = { "/usr/bin/librewolf", NULL };
 static const char *passmenucmd[]  = { "/usr/bin/passmenu", NULL };
 
@@ -74,10 +74,10 @@ static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_p,	   spawn,          {.v = passmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_s, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = slockcmd } },
-	{ MODKEY|ShiftMask,             XK_b,      spawn,          {.v = qutecmd } },
-	{ MODKEY|ShiftMask,             XK_i,      spawn,          {.v = librecmd } },
+	/*{ MODKEY|ShiftMask,             XK_b,      spawn,          {.v = qutecmd } },*/
+	{ MODKEY|ShiftMask,             XK_b,      spawn,          {.v = librecmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
